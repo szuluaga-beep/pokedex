3e1c8a2d9b65ca18f61a83a7382d9eb85df8678f
Steven Zuluaga <steven000069@hotmail.com>
2022-08-22 22:44:07 -0500

    Como mostrar info de la API

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,10 +5,12 @@ const next = document.querySelector("#next");
 
 let max = 8;
 let min = 1;
-
+//funcion para consumir api de pokemones
 const fetchPokemon = async(id) => {
      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-     console.log(response)
+    // console.log(response)
+     const data = await response.json()
+     console.log(data)
 }
 
 fetchPokemon(min)
