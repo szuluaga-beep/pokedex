fff9b9eda6d0fa19575ce1ceed8f17721a6a504c
Steven Zuluaga <steven000069@hotmail.com>
2022-08-22 22:52:04 -0500

    mostrar nombre de pokemones en la pagina

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,11 +10,21 @@ const fetchPokemon = async (id) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
   // console.log(response)
   const data = await response.json();
-  console.log(data);
+  createPokemon(data)
 };
 const fetchPokemons = (min, max) => {
      for (let index = min; index <= min + max; index++) {
        fetchPokemon(index)
   }
 };
+
+const createPokemon = (poke) => {
+     const card = document.createElement('div')
+     card.classList.add('card')
+     const name = document.createElement('p')
+     name.innerHTML = poke.name
+
+     card.appendChild(name)
+     pokemContainer.appendChild(card)
+}
 fetchPokemons(min, max);
