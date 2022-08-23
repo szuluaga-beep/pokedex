80b9d9d547e85779dc7f7641f9f91b5b72c51d33
Steven Zuluaga <steven000069@hotmail.com>
2022-08-22 22:46:51 -0500

    como obtener nueve pokemones

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,11 +6,15 @@ const next = document.querySelector("#next");
 let max = 8;
 let min = 1;
 //funcion para consumir api de pokemones
-const fetchPokemon = async(id) => {
-     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-    // console.log(response)
-     const data = await response.json()
-     console.log(data)
-}
-
-fetchPokemon(min)
+const fetchPokemon = async (id) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  // console.log(response)
+  const data = await response.json();
+  console.log(data);
+};
+const fetchPokemons = (min, max) => {
+     for (let index = min; index <= min + max; index++) {
+       fetchPokemon(index)
+  }
+};
+fetchPokemons(min, max);
