16538e8f9e18a25d3622f634dc5e7ea9102e3278
Steven Zuluaga <steven000069@hotmail.com>
2022-08-22 22:59:12 -0500

    Agregar imagen de pokemon

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -21,10 +21,16 @@ const fetchPokemons = (min, max) => {
 const createPokemon = (poke) => {
      const card = document.createElement('div')
      card.classList.add('card')
+     card.classList.add('shadow')
      const name = document.createElement('p')
      name.innerHTML = poke.name
 
+     const imagePoke = document.createElement('img')
+     imagePoke.src = poke.sprites.other.home.front_default;
+     imagePoke.classList.add('card-img-top')
+
      card.appendChild(name)
+     card.appendChild(imagePoke)
      pokemContainer.appendChild(card)
 }
 fetchPokemons(min, max);
